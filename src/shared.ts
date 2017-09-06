6f4c789690b8d61e409fc7c462c2c747d3d201d1
Rich Harris <richard.a.harris@gmail.com>
2017-09-06 10:48:30 -0400

    undo tweak

--- src/shared.ts
+++ src/shared.ts
@@ -1,4 +1,3 @@
 export function isDate(obj) {
-	return obj instanceof Date;
-	// return Object.prototype.toString.call(obj) === '[object Date]';
+	return Object.prototype.toString.call(obj) === '[object Date]';
 }
